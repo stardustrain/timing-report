453448a452e9f1e4b5b308983bddd569ee26d1db
stardustrain <kthanterran@gmail.com>
2020-02-19 22:00:51 +0900

    Initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    semi: ['warn', 'never'],
+    'sort-imports': ['off'],
+    quotes: ['warn', 'single'],
+    'no-extra-boolean-cast': ['off'],
+    'react/prop-types': ['off'],
+    'react/jsx-boolean-value': ['off'],
+    'react/display-name': ['off'],
+    '@typescript-eslint/member-delimiter-style': [
+      'warn',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false,
+        },
+        singleline: {
+          requireLast: false,
+        },
+      },
+    ],
+    '@typescript-eslint/no-empty-interface': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-non-null-assertion': ['off'],
+    '@typescript-eslint/interface-name-prefix': ['warn'],
+  },
+}
